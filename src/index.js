fe588448cb925b7faad7058a4704224bdbea186c
lgants <lgants@gmail.com>
2017-11-28 23:00:58 -0600

    fixed imports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
-import store from './models/user-store';
+import store from './models/UserStore';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App store={store} />, document.querySelector('.container'));
 registerServiceWorker();
